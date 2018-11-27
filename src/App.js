9aafbf67a561b6dd1d841c174267c367f7f86fb5
satishmaurya <satishmaurya1992@gmail.com>
2018-11-27 22:29:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person.js';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons:[
+      {name:'Max',age:28},
+      {name:'Manu',age:26},
+      {name:'John',age:24}
+
+    ],
+    otherState: 'Some value'
+  }
+  switchNameHandler = () => {
+    //console.log('clicked');
+    this.setState({
+      persons:[
+      {name:'Maxmillion',age:28},
+      {name:'Manu',age:26},
+      {name:'John cena',age:64}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
-      <h1>Indiamart Web App</h1>
-      <p> Something goes wrong!!</p>
-      <Person/>
+       <h1> Indiamart Web App</h1>
+       <button onClick={this.switchNameHandler}>Switch Name </button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+       
       </div>
     );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m Indiamart!!'));*
+   // return React.createElement('div', {className:'App'},React.createElement('h1', null, 'Does is this work now?'));
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const person = () => {
-	return <p>I am a New User here!! </p>
-};
+const person = (xyz) => {
+	return (
+		<div>
+		<p>I am a {xyz.name} and I am {xyz.age} years old</p>			
+		<p>{xyz.children}</p>
+		</div>
+		)
+}
+
 export default person;
